e142817a40d536c895381502d8277fe085d135f6
halak0013 <zumruduanka0013@gmail.com>
2023-05-16 23:53:19 +0300

    fixing similarity bug and adding test data

--- src/main/java/com/mycompany/heap/test/ZeroList.java
+++ src/main/java/com/mycompany/heap/test/ZeroList.java
@@ -22,32 +22,70 @@ public class ZeroList {
         count++;
     }
 
+    public void print() {
+        Node current = root;
+        while (current != null) {
+            System.out.print(current.data + " " + current.index + " - ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
     public static double calculateCosSim(ZeroList main_vector, ZeroList target_vector) {
         // ? vektörlerin çarpımı / vektörlerin uzunluklarının çarpımı
         // main : [1,1,1,0,0,1]
-                //1,2,3,6
+        // 1,2,3,6
         // target: [0,1,1,1,1,1]
-                //2,3,4,5,6
+        // 2,3,4,5,6
         Node cur_main = main_vector.root;
         Node cur_target = target_vector.root;
         double mul_of_vector = 0;
         double v_m_dis = 0;
         double v_t_dis = 0;
+
         while (cur_main != null) {
             while (cur_main != null && cur_target != null && cur_main.index != cur_target.index) {
-                v_m_dis+=cur_main.data*cur_main.data;
-                v_t_dis+=cur_target.data*cur_target.data;
-                if(cur_main.index<cur_target.index)
-                    cur_main=cur_main.next;
-                else
-                    cur_target=cur_target.next;
-            }
-            mul_of_vector+=cur_main.data*cur_target.data;
-            v_m_dis+=cur_main.data*cur_main.data;
-            v_t_dis+=cur_target.data*cur_target.data;
-        }
 
-        return mul_of_vector/(Math.sqrt(v_m_dis) *Math.sqrt(v_t_dis));
+                if (cur_main.index < cur_target.index) {
+                    v_m_dis += cur_main.data * cur_main.data;
+                    cur_main = cur_main.next;
+                } else {
+                    v_t_dis += cur_target.data * cur_target.data;
+                    cur_target = cur_target.next;
+                }
+            }
+            
+            mul_of_vector += cur_main.data * cur_target.data;
+            v_m_dis += cur_main.data * cur_main.data;
+            v_t_dis += cur_target.data * cur_target.data;
+            cur_main = cur_main.next;
+            cur_target = cur_target.next;
+        }
+        System.out.println(v_m_dis + " " + v_t_dis + " " + mul_of_vector);
+
+        return mul_of_vector / (Math.sqrt(v_m_dis) * Math.sqrt(v_t_dis));
     }
 
+    public static void main(String[] args) {
+        ZeroList z1 = new ZeroList();
+        ZeroList z2 = new ZeroList();
+        z1.insert(1);
+        z1.insert(1);
+        z1.insert(1);
+        z1.insert(0);
+        z1.insert(0);
+        z1.insert(1);
+
+        z2.insert(0);
+        z2.insert(1);
+        z2.insert(1);
+        z2.insert(1);
+        z2.insert(1);
+        z2.insert(1);
+
+        z1.print();
+        z2.print();
+
+        System.out.println(calculateCosSim(z1, z2));
+    }
 }
