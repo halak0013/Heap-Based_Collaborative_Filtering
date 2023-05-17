aa7f23649490420ac8735fb0826a73ff4101c5eb
halak0013 <zumruduanka0013@gmail.com>
2023-05-17 19:49:09 +0300

    spacial csv reader

--- src/main/java/com/mycompany/heap/test/csvRead.java
+++ src/main/java/com/mycompany/heap/test/csvRead.java
@@ -4,25 +4,70 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class csvRead {
-    public static void readFile(String path) {
+public class csvRead<C> {
+    public void readFile(String path, ZeroList<C> result) {
 
         String line = "";
         String csvDelimiter = ",";
-        
+        boolean pass = true;
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             while ((line = br.readLine()) != null) {
-                String[] data = line.split(csvDelimiter);
-                for (int i = 1; i < data.length; i++) {
-                    
+                if (pass) {
+                    pass = false;
+                    continue;
                 }
-                //System.out.println(data[0]);
+                String[] data = line.split(csvDelimiter);
+                ZeroList<C> tmpZL = new ZeroList<>();
+                for (int i = 0; i < data.length; i++) {
+                    tmpZL.insert(i);
+                }
+                // System.out.println(data[0]);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    public void readFileInt(String path, ZeroList<ZeroList<Integer>> result) {
+
+        String line = "";
+        String csvDelimiter = ",";
+        boolean pass = true;
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            while ((line = br.readLine()) != null) {
+                if (pass) {
+                    pass = false;
+                    continue;
+                }
+                String[] data = line.split(csvDelimiter);
+                ZeroList<Integer> tmpZL = new ZeroList<>();
+                for (int i = 0; i < data.length; i++) {
+                    tmpZL.insert(Integer.parseInt(data[i]));
+                }
+                result.insert(tmpZL);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void print(ZeroList<ZeroList<C>> zl) {
+        Node<ZeroList<C>> tmo = zl.root;
+        while (tmo != null) {
+            Node<C> tmi = tmo.data.root;
+            while(tmi != null) {
+                System.out.print(tmi.data+" - ");
+                tmi=tmi.next;
+            }
+            tmo = tmo.next;
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        readFile("./data/target_user.csv");
+        ZeroList<ZeroList<Integer>> z1 = new ZeroList<>();
+        csvRead<Integer> c = new csvRead<>();
+        c.readFileInt("./data/target_user.csv", z1);
+        c.print(z1);
     }
 }
