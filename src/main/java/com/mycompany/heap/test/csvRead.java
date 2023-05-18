9c019a4bdf2c62163b3770fb9fdd826e997632ba
halak0013 <zumruduanka0013@gmail.com>
2023-05-19 00:11:55 +0300

    insert elemant on combobox method

--- src/main/java/com/mycompany/heap/test/csvRead.java
+++ src/main/java/com/mycompany/heap/test/csvRead.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import javax.swing.DefaultComboBoxModel;
+
 public class csvRead<C> {
     public void readFile(String path, ZeroList<ZeroList<C>> result) {
 
@@ -19,7 +21,7 @@ public class csvRead<C> {
                 String[] data = line.split(csvDelimiter);
                 ZeroList<C> tmpZL = new ZeroList<>();
                 for (int i = 0; i < data.length; i++) {
-                    tmpZL.insert((C)data[i]);
+                    tmpZL.insert((C) data[i]);
                 }
                 result.insert(tmpZL);
             }
@@ -55,12 +57,24 @@ public class csvRead<C> {
         Node<ZeroList<C>> tmo = zl.root;
         while (tmo != null) {
             Node<C> tmi = tmo.data.root;
-            while(tmi != null) {
-                System.out.print(tmi.data+" - ");
-                tmi=tmi.next;
+            while (tmi != null) {
+                System.out.print(tmi.data + " - ");
+                tmi = tmi.next;
             }
             tmo = tmo.next;
             System.out.println();
+        }
+    }
+
+    void insert(ZeroList<ZeroList<C>> zl, int index, DefaultComboBoxModel<String> model) {
+        Node<ZeroList<C>> tmo = zl.root;
+        while (tmo != null) {
+            Node<C> tmi = tmo.data.root;
+            for (int i = 0; i < index; i++) {
+                tmi = tmi.next;
+            }
+            model.addElement(tmi.data.toString()+". User");
+            tmo = tmo.next;
         }
     }
 
