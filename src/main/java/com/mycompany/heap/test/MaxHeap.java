77d7af752fc9094c621b6ac777b598043ebdbb05
halak0013 <zumruduanka0013@gmail.com>
2023-05-19 00:12:11 +0300

    bug fixing

--- src/main/java/com/mycompany/heap/test/MaxHeap.java
+++ src/main/java/com/mycompany/heap/test/MaxHeap.java
@@ -12,7 +12,9 @@ public class MaxHeap<T extends Comparable<T>> {
 
     T[] heap;
     int size;
-
+//1 2 3 4 5 6 7 8 index
+//6 8 2 4 3 8 1 2 uzaklık
+//3 8 1 2
     public MaxHeap(int capacity) {
         heap = (T[]) new Comparable[capacity];
         size = 0;
@@ -29,21 +31,46 @@ public class MaxHeap<T extends Comparable<T>> {
     }
 
     void insert(T data) {
-
+        int current;
         if (size < heap.length) {
 
             heap[size] = data;
-            int current = size++;
+            current = size++;
 
             while (heap[current].compareTo(heap[parent(current)]) > 0) {
                 swap(current, parent(current));
                 current = parent(current);
             }
-
+//! gelen elemanın currentı topupHeapify kontol edilmesi gerekebilir
         } else {
             System.out.println("Heap is full!!!");
+            current = size;
+            int min = (int)heap[0];
+            while(heap[current].compareTo(heap[parent(current)]) < 0){
+                min = (int) heap[current];
+                topupHeapify(min);
+            }
         }
     }
+
+    void topupHeapify(int idx) {
+        int left = 2*idx + 1;
+        int right = 2*idx + 2;
+
+        int max = idx;
+        if (left < size && heap[left].compareTo(heap[max]) > 0) {
+            max = left;
+        }
+        if (right < size && heap[right].compareTo(heap[max]) > 0) {
+            max = right;
+        }
+        if (max!= idx) {
+            swap(idx, max);
+            topupHeapify(max);
+        }
+    }  
+
+
 
     void print() {
 
