325e10b3b1decc162f25381ad48059c6ead763d3
halak0013 <zumruduanka0013@gmail.com>
2023-05-21 15:40:21 +0300

    bug fixing

---
+++ src/main/java/com/mycompany/heap/test/CosSimilarity.java
@@ -0,0 +1,52 @@
+package com.mycompany.heap.test;
+
+public class CosSimilarity {
+    private double length(ZeroList<Integer> vector) {
+        Node<Integer> cur = vector.root;
+        double length = 0;
+        while (cur != null) {
+            length += cur.data * cur.data;
+            cur = cur.next;
+        }
+        return length;
+    }
+
+    private double dotProduct(ZeroList<Integer> main_vector, ZeroList<Integer> target_vector) {
+        Node<Integer> cur_m = main_vector.root;
+        Node<Integer> cur_t = target_vector.root;
+        double dotProduct = 0;
+        while (cur_m != null && cur_t != null) {
+            if (cur_m.index < cur_t.index) {
+                cur_m = cur_m.next;
+            } else if (cur_m.index > cur_t.index) {
+                cur_t = cur_t.next;
+            } else {
+                dotProduct += cur_m.data * cur_t.data;
+                cur_m = cur_m.next;
+                cur_t = cur_t.next;
+            }
+        }
+
+        return dotProduct;
+    }
+
+    public double calculateCosSim(ZeroList<Integer> main_vector, ZeroList<Integer> target_vector) {
+        // ? vektörlerin çarpımı / vektörlerin uzunluklarının çarpımı
+        // main : [1,1,1,0,0,1]
+        // 1,2,3,6
+        // target: [0,1,1,1,1,1]
+        // 2,3,4,5,6
+        double mul_of_vector = dotProduct(main_vector, target_vector);
+        double v_m_dis = length(main_vector);
+        double v_t_dis = length(target_vector);
+        // 1,1,1,0,0,1 -> 1,1,1,1
+        // 0,1,2,3,4,5 0,1,2,5
+
+        // 0,1,1,1,1,0 -> 1,1,1,1
+        // 1,2,3,4
+
+        System.out.println(v_m_dis + " " + v_t_dis + " " + mul_of_vector);
+
+        return mul_of_vector / (Math.sqrt(v_m_dis) * Math.sqrt(v_t_dis));
+    }
+}
